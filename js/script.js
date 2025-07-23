e3b1c071cca46cb03ba6641d400276daa633b778
Mohannad Hassan <mohannadhassan616@gmail.com>
2025-07-24 00:07:01 +0300

    script.js

--- js/script.js
+++ js/script.js
@@ -173,22 +173,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
-
-// Simple scroll to top button
-const scrollToTopBtn = document.querySelector('.scroll-to-top');
-if (scrollToTopBtn) {
-    window.addEventListener('scroll', () => {
-        if (window.pageYOffset > 300) {
-            scrollToTopBtn.style.display = 'block';
-        } else {
-            scrollToTopBtn.style.display = 'none';
-        }
-    });
-
-    scrollToTopBtn.addEventListener('click', () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    });
-}
